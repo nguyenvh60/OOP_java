47ff9f2c3b4c8f5ecd36ac62ef69de9a1dde15f0
nguyenvh60 <contact.tkl.fco@gmail.com>
2025-05-30 17:08:33 +0700

    OOP

---
+++ src/Bai5_Modifiers/Main.java
@@ -0,0 +1,11 @@
+package Bai5_Modifiers;
+
+public class Main {
+    public String name = "Nguyên Hoàng";
+    private String ig = "nguyen_60seconds" ;
+
+    public static void main(String[] args) {
+        Main main = new Main();
+        System.out.println(main.ig);
+    }
+}
---
+++ src/Bai5_Modifiers/Second.java
@@ -0,0 +1,11 @@
+package Bai5_Modifiers;
+
+public class Second {
+    public static void main(String[] args) {
+        Main main = new Main();
+        System.out.println(main.name);
+
+        Main main1 = new Main();
+//        System.out.println(main1.ig); // Không thể vì ig ở Class Main
+    }
+}
